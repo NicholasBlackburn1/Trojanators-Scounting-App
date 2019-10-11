63f5dd91306ba690404674d96f444070c1df9c0d
Nicholas Blackburn <nickblackburn02@gmail.com>
2019-10-11 08:52:07 -0400

    added Simple Main page
    Creation of Main app page with an Header ui

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MainPage } from "MainPage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+     <MainPage/>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- MainPage.js
+++ MainPage.js
@@ -1,15 +1,32 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MaterialHeader2 from "./components/MaterialHeader2";
 
-import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    SectionList,
-} from 'react-native';
-import Constants from 'expo-constants';
-
-export default class MainMenu extends Component{
-
-    
+export default class MainWindow extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.rect}>
+                    <MaterialHeader2 style={styles.materialHeader2} />
+                </View>
+            </View>
+        );
     }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 375,
+        height: 812
+    },
+    rect: {
+        width: 375,
+        height: 812,
+        backgroundColor: "rgba(169, 169, 169,1)"
+    },
+    materialHeader2: {
+        width: 375,
+        height: 56,
+        marginTop: 31
+    }
+});
